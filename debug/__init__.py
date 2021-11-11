b1a140d9536c930a5abc3b556d825373b7b79f6d
krunch3r76 <biz@u26a4.com>
2021-11-11 11:46:44 -0800

    add debug logging package

---
+++ debug/__init__.py
@@ -0,0 +1,32 @@
+import os,sys,inspect
+from pathlib import Path, PurePath
+
+TEXT_COLOR_MAGENTA="\033[35;1m"
+TEXT_COLOR_DEFAULT="\033[0m"
+
+def get_calling_script():
+    return inspect.stack()[1][1]
+
+def dlog(msg, debug_level=1, color=TEXT_COLOR_MAGENTA):
+
+    def _get_calling_script():
+        path_string = inspect.stack()[2][1]
+        parts = PurePath(path_string).parts
+        if len(parts) > 2:
+            path_string='/'.join(parts[-2:])
+        else:
+            path_string='/'.join(parts)
+        # home_path_string = str(Path.home())
+        # return path_string.replace(home_path_string, "")
+        return path_string
+
+    def _get_calling_script_lineno():
+        return inspect.stack()[2][2]
+
+
+    _DEBUGLEVEL = int(os.environ['PYTHONDEBUGLEVEL']) if 'PYTHONDEBUGLEVEL' in os.environ else 0
+
+    if debug_level <= _DEBUGLEVEL:
+        print(f"\n[{_get_calling_script()}:{_get_calling_script_lineno()}] "
+            f"{color}{msg}{TEXT_COLOR_DEFAULT}\n", file=sys.stderr)
+        # print(f"\n[model.py] {utils.TEXT_COLOR_MAGENTA}{msg}{utils.TEXT_COLOR_DEFAULT}\n", file=sys.stderr)
