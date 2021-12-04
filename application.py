d8bc21dcc79095d5bad870f71871c3c23dd4b0fa
krunch3r76 <biz@u26a4.com>
2021-12-03 18:24:48 -0800

    add usage on command line

--- application.py
+++ application.py
@@ -8,10 +8,8 @@ import sys
 import debug
 
 if __name__ == "__main__":
-    ip="localhost"
-    port=8000
-
     if len(sys.argv) == 1:
+        # <script>
         # configure for controller to use local connection
         from model.offer_lookup import OfferLookup
         print(f"\033[1mrunning locally\033[0m")
@@ -24,15 +22,20 @@ if __name__ == "__main__":
         controller_process.start()
 
         appView()
-    else:
         # parse arguments
         # <remote server or client ip> <remote server port>
-        if len(sys.argv) >=3:
-            ip=sys.argv[2]
+    elif len(sys.argv) >=2:
+        # <script> <client|serve> <host> [<port>]
+            if len(sys.argv) >=3:
+                ip=sys.argv[2]
+            else:
+                ip="localhost"
+
             if len(sys.argv) == 4:
                 port=int(sys.argv[3])
+            else:
+                port=8000
 
-        if len(sys.argv) > 1:
             if sys.argv[1]=="serve":
                 from model import run_server
                 from model.offer_lookup import OfferLookup
@@ -40,7 +43,7 @@ if __name__ == "__main__":
                 # _log_msg(f"[application.py] launching server on {ip}:{port}")
                 print(f"\033[1mlaunching server on {ip}:{port}\033[0m")
                 run_server(ip, port)
-            
+
             elif sys.argv[1]=="client":
                 from controller.local_connection import LocalConnection
                 print(f"\033[1mlaunching client to connect with {ip}:{port}\033[0m")
@@ -52,6 +55,5 @@ if __name__ == "__main__":
                 # freeze_support()
                 controller_process.start()
                 appView()
-
-            else:
-                print("invalid arguments", file=sys.stderr)
+    else:
+        print(f"usage: {} <client|serve> [<host=localhost>] [<port=8000>]")
