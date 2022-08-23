ef84a987a29b885a5fbde3ee1230342b80d31776
krunch3r76 <biz@u26a4.com>
2022-08-22 20:12:23 -0700

    ordering paradigm

--- gc__listoffers/widgets/providertree.py
+++ gc__listoffers/widgets/providertree.py
@@ -26,46 +26,60 @@ def _measure(text, window=None):
 
 class InsertDict(UserDict):
     # notes, features will be used to compose a full list of unique features filterable
-    def __init__(self, merge: InsertionTuple):
+    def __init__(self, merge: InsertionTuple, column_defs, column_placements=None):
 
         # representation of columns that may be inserted into the view, for export
         # to controller
         self.features = merge.features # revise to use filtered, and make as list
-        super().__init__({
-            'rowId' : merge.offerRowID,
-            'paymentForm' : merge.token_kind,
-            'features' : self.features,# revise to curtail
-            'name' : merge.name,
-            'address' : merge.address,
-            'cpu (/hr)' : str(float(merge.cpu_sec)*3600.0),
-            'dur (/hr)' : str(float(merge.duration_sec)*3600.0),
-            'start' : merge.fixed,
-            'cores' : merge.cores,
-            'threads' : merge.threads,
-            'frequency' : merge.freq,
-            'version' : merge.version,
-            'mem' : merge.mem_gib,
-            'storage' : merge.storage_gib
-                })
+        super().__init__()
+        # {
+        #     'rowId' : merge.offerRowID,
+        #     'paymentForm' : merge.token_kind,
+        #     'features' : self.features,# revise to curtail
+        #     'name' : merge.name,
+        #     'address' : merge.address,
+        #     'cpu (/hr)' : str(float(merge.cpu_sec)*3600.0),
+        #     'dur (/hr)' : str(float(merge.duration_sec)*3600.0),
+        #     'start' : merge.fixed,
+        #     'cores' : merge.cores,
+        #     'threads' : merge.threads,
+        #     'frequency' : merge.freq,
+        #     'version' : merge.version,
+        #     'mem' : merge.mem_gib,
+        #     'storage' : merge.storage_gib
+        #         })
         # super().__init__({
         #         'rowId': None, 'name': None, 'address': None, 'cpu (/hr)': None, 'dur (/hr)': None,
         #         'cores': None, 'threads': None, 'frequency': None, 'version': None, 'mem': None,
         #         'storage': None, 'features': None
         #         })
-        # self['rowId'] = merge.offerRowID
-        # self['paymentForm'] = merge.token_kind
-        # self['features'] = self.features # revise to curtail
-        # self['name'] = merge.name
-        # self['address'] = merge.address
-        # self['cpu (/hr)'] = str(float(merge.cpu_sec)/3600.0)
-        # self['dur (/hr)'] = str(float(merge.duration_sec)/3600.0)
-        # self['start'] = merge.fixed
-        # self['cores'] = merge.cores
-        # self['threads'] = merge.threads
-        # self['frequency'] = merge.freq
-        # self['mem'] = merge.mem_gib
-        # self['storage'] = merge.storage_gib
-        # self.update(merge)
+
+        tdict = dict()
+        tdict['rowId'] = merge.offerRowID
+        tdict['paymentForm'] = merge.token_kind
+        tdict['features'] = self.features # revise to curtail
+        tdict['name'] = merge.name
+        tdict['address'] = merge.address
+        tdict['cpu (/hr)'] = str(float(merge.cpu_sec)/3600.0)
+        tdict['dur (/hr)'] = str(float(merge.duration_sec)/3600.0)
+        tdict['start'] = merge.fixed
+        tdict['cores'] = merge.cores
+        tdict['threads'] = merge.threads
+        tdict['frequency'] = merge.freq
+        tdict['version'] = merge.version
+        tdict['mem'] = merge.mem_gib
+        tdict['storage'] = merge.storage_gib
+        column_defs_as_list = list(column_defs)[1:]
+        print()
+        for offset in column_placements:
+            key_at_placement = column_defs_as_list[offset]
+            print(key_at_placement)
+            self[key_at_placement] = tdict[key_at_placement]
+        print()
+        print(column_defs_as_list)
+
+        
+#        self.update(merge)
 
 def insert_dict_from_tuple(insertionTuple):
     """
@@ -123,6 +137,8 @@ class ProviderTree(ttk.Frame):
             'mem': { },
             'storage': { },
             }
+        self.column_placements = [ i for i in range(len(list(self.column_defs))-1) ]
+                                
         column_defs_dict = dict(self.column_defs.items())
         column_defs_dict.pop('#0')
         for key, value in column_defs_dict.items():
@@ -159,9 +175,9 @@ class ProviderTree(ttk.Frame):
                                    insert row
         """
         insertionTuple = InsertionTuple(**rowdict)
-        print(insertionTuple)
+        # print(insertionTuple)
         self.insertionTuples.append(insertionTuple)
-        insertionDict = InsertDict(insertionTuple) 
+        insertionDict = InsertDict(insertionTuple, self.column_defs, self.column_placements) 
         self.treeview.insert('', 'end', iid=insertionDict['rowId'], values=list(insertionDict.data.values()))
         # pop features
         # self.treeview.insert('', 'end', iid=rowvalues['rowId'], values=list(rowvalues.values()))
